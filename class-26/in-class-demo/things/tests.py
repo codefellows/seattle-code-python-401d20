574104582c99a7bf4044eb16de3ebbca194b5e13
Adam Owada <adamowada@gmail.com>
2023-01-23 11:30:55 -0800

    context info

--- class-26/in-class-demo/things/tests.py
+++ class-26/in-class-demo/things/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class ThingsTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        print(f'the url is: {url}')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
+
