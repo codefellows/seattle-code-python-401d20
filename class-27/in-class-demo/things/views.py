af456ce03d4052f6f209c2098da34d55631da69f
Adam Owada <adamowada@gmail.com>
2023-01-24 10:55:30 -0800

    demo code progress 2

--- class-27/in-class-demo/things/views.py
+++ class-27/in-class-demo/things/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["things"] = [
+            {
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/b/b3/SoilRake.jpg",
+                "title": "Rake",
+                "description": "Better than a shovel or a broom for leaves. Like a pitch fork but less pokey.",
+                "reference_url": "https://en.wikipedia.org/wiki/Rake_(tool)"
+            }, {
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/9/92/Soup_Spoon.jpg",
+                "title": "Spoon",
+                "description": "An eating utensil. Better for soup and cereal than a fork.",
+                "reference_url": "https://en.wikipedia.org/wiki/Spoon"
+            }, {
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Spoon_Piknik_i_Parken_2017_%28175930%29.jpg/600px-Spoon_Piknik_i_Parken_2017_%28175930%29.jpg",
+                "title": "Spoon",
+                "description": "Spoon is also an American rock band from Austin, Texas.",
+                "reference_url": "https://en.wikipedia.org/wiki/Spoon_(band)"
+            },
+        ]
+
+        return context
