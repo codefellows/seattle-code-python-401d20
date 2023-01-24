8adfb73cee3d91d881e2570b27d9030366e5ef11
Adam Owada <adamowada@gmail.com>
2023-01-24 12:01:25 -0800

    demo code 4

--- class-27/in-class-demo/things/models.py
+++ class-27/in-class-demo/things/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Thing(models.Model):
+    name = models.CharField(max_length=256)
+    rating = models.IntegerField(default=0)
+    reviewer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
