c7255ec48014fb31b0af1024973a8a36fb5602db
Adam Owada <adamowada@gmail.com>
2023-01-25 11:17:00 -0800

    added cc starter code

---
+++ class-28/code-challenge-starter-code/movies.py
@@ -0,0 +1,56 @@
+movies = [
+  {
+    title: "Beetlejuice",
+    year: 1988,
+    genres: ["Comedy", "Fantasy"],
+  },
+  {
+    title: "The Cotton Club",
+    year: 1984,
+    genres: ["Crime", "Drama", "Music"],
+  },
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    genres: ["Crime", "Drama"],
+  },
+  {
+    title: "Crocodile Dundee",
+    year: 1986,
+
+    genres: ["Adventure", "Comedy"],
+  },
+  {
+    title: "Valkyrie",
+    year: 2008,
+    genres: ["Drama", "History", "Thriller"],
+  },
+  {
+    title: "Ratatouille",
+    year: 2007,
+    genres: ["Animation", "Comedy", "Family"],
+  },
+  {
+    title: "City of God",
+    year: 2002,
+
+    genres: ["Crime", "Drama"],
+  },
+  {
+    title: "Memento",
+    year: 2000,
+
+    genres: ["Mystery", "Thriller"],
+  },
+  {
+    title: "The Intouchables",
+    year: 2011,
+
+    genres: ["Biography", "Comedy", "Drama"],
+  },
+  {
+    title: "Stardust",
+    year: 2007,
+    genres: ["Adventure", "Family", "Fantasy"],
+  },
+]
---
+++ class-28/code-challenge-starter-code/test.py
@@ -0,0 +1,5 @@
+# Expected test output of test #1
+expected1 = ["The Intouchables", "Valkyrie", "Stardust", "Ratatouille", "City of God", "Memento", "The Shawshank Redemption", "Beetlejuice", "Crocodile Dundee", "The Cotton Club"]
+
+# Expected test output of test #2
+expected2 = ["Beetlejuice", "City of God", "The Cotton Club", "Crocodile Dundee", "The Intouchables", "Memento", "Ratatouille", "The Shawshank  edemption", "Stardust", "Valkyrie"];
