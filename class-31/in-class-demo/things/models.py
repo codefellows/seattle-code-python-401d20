40d1f144cdbeb638a622afe64d942ec6bd72f298
Adam Owada <adamowada@gmail.com>
2023-01-30 10:37:49 -0800

    demo 1

---
+++ class-31/in-class-demo/things/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+class Thing(models.Model):
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+
+    # on creation
+    create_at = models.DateTimeField(auto_now_add=True)
+    # on save
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
