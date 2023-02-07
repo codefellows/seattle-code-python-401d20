d097a639835bc1afd6d4c8a7eaf0d59500c5b067
Adam Owada <adamowada@gmail.com>
2023-02-07 11:27:42 -0800

    demo 2

---
+++ class-37/expert-eight-ball/pages/careers.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Careers() {
+    return (
+        <div className='flex flex-col items-center justify-center h-screen bg-gray-100'>
+            <h1 className="text-4xl"> Careers Page Coming Soon</h1>
+            <Link href="/" className="p-4 m-4 text-2xl bg-gray-300 rounded-lg">Back to Home</Link>
+        </div>
+    );
+}
