24da7e2b06805ad010b2fb843919e39d385a65b4
Adam Owada <adamowada@gmail.com>
2023-02-07 11:38:20 -0800

    demo 2

--- class-37/expert-eight-ball/pages/index.js
+++ class-37/expert-eight-ball/pages/index.js
@@ -3,6 +3,12 @@ import Link from 'next/link';
 
 
 export default function Home() {
+
+    function questionAskedHandler(event) {
+        event.preventDefault();
+        alert(event.target.question.value);
+    }
+
     return (
         <>
             <Head>
@@ -11,14 +17,25 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+
             <header className='flex items-center justify-between p-4 bg-gray-500 text-gray-50'>
                 <h1>Expert 8 Ball</h1>
                 <p>1 questions answered</p>
             </header>
+
             <main className=''>
-
-
+                <form className="flex w-1/2 p-2 mx-auto my-4 bg-gray-200" onSubmit={questionAskedHandler}>
+                    <input name="question" className="flex-auto pl-1" />
+                    <button className="px-2 py-1 bg-gray-500 text-gray-50">Ask</button>
+                </form>
+                {/* Eight Ball */}
+                <div className="w-96 h-96 mx-auto my-4 bg-gray-900 rounded-full">
+                    <div className="relative flex items-center justify-center w-48 h-48 rounded-full bg-gray-50 top-16 left-16">
+                        <p className="text-xl text-center">Ask A Question!</p>
+                    </div>
+                </div>
             </main>
+
             <footer className='p-4 mt-8 bg-gray-500 text-gray-50'>
                 <Link href="/careers">
                     Careers
