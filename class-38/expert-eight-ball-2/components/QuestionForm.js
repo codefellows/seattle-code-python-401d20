9fb00b468a8e1145ef28c320f0e77f74ba04d017
Adam Owada <adamowada@gmail.com>
2023-02-08 10:44:43 -0800

    code updated

---
+++ class-38/expert-eight-ball-2/components/QuestionForm.js
@@ -0,0 +1,11 @@
+export default function QuestionForm(props) {
+    return (
+        <form
+            className="flex w-1/2 p-2 mx-auto my-4 bg-gray-200"
+            onSubmit={questionAskedHandler}
+        >
+            <input name="question" className="flex-auto pl-1" />
+            <button className="px-2 py-1 bg-gray-500 text-gray-50">Ask</button>
+        </form>
+    );
+}
