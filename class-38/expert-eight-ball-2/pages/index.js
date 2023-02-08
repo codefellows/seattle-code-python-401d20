1272c2c1c72f5e968bd8b7158951730e2353bec3
Adam Owada <adamowada@gmail.com>
2023-02-08 10:48:53 -0800

    code updated

--- class-38/expert-eight-ball-2/pages/index.js
+++ class-38/expert-eight-ball-2/pages/index.js
@@ -50,11 +50,7 @@ export default function Home() {
             <Header answeredQuestions={answeredQuestions} />
 
             <main className=''>
-                {/* Form section */}
-                {/* <form className="flex w-1/2 p-2 mx-auto my-4 bg-gray-200" onSubmit={questionAskedHandler}>
-                    <input name="question" className="flex-auto pl-1" />
-                    <button className="px-2 py-1 bg-gray-500 text-gray-50">Ask</button>
-                </form> */}
+                
                 <QuestionForm questionAskedHandler={questionAskedHandler} />
 
                 {/* Eight Ball */}
