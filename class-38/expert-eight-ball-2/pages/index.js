15144f3415be785e8aefcf155b2675008ae6098a
Adam Owada <adamowada@gmail.com>
2023-02-08 11:12:51 -0800

    code updated

--- class-38/expert-eight-ball-2/pages/index.js
+++ class-38/expert-eight-ball-2/pages/index.js
@@ -53,34 +53,10 @@ export default function Home() {
             <Header answeredQuestions={answeredQuestions} />
 
             <main className=''>
-
                 <QuestionForm questionAskedHandler={questionAskedHandler} />
 
                 <EightBall getLatestReply={getLatestReply} />
 
-                {/* Table of Responses */}
-                {/* <table className="w-1/2 mx-auto my-4">
-                    <thead>
-                        <tr>
-                            <th className="border border-gray-700">No.</th>
-                            <th className="border border-gray-700">Question</th>
-                            <th className="border border-gray-700">Response</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            answeredQuestions.map((item) => {
-                                return (
-                                    <tr key={item.id}>
-                                        <td className="pl-2 border border-gray-700">{item.id}</td>
-                                        <td className="pl-2 border border-gray-700">{item.question}</td>
-                                        <td className="pl-2 border border-gray-700">{item.reply}</td>
-                                    </tr>
-                                );
-                            })
-                        }
-                    </tbody>
-                </table> */}
                 <ResponseTable answeredQuestions={answeredQuestions} />
             </main>
 
