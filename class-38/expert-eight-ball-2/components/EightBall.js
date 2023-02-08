4504356e477ad9eab2223d3248f9bb331ba044d6
Adam Owada <adamowada@gmail.com>
2023-02-08 11:32:09 -0800

    code updated

--- class-38/expert-eight-ball-2/components/EightBall.js
+++ class-38/expert-eight-ball-2/components/EightBall.js
@@ -1,8 +1,8 @@
-export default function EightBall(props) {
+export default function EightBall({ getLatestReply }) {
     return (
         <div className="w-96 h-96 mx-auto my-4 bg-gray-900 rounded-full">
             <div className="relative flex items-center justify-center w-48 h-48 rounded-full bg-gray-50 top-16 left-16">
-                <p className="text-xl text-center">{props.getLatestReply()}</p>
+                <p className="text-xl text-center">{getLatestReply()}</p>
             </div>
         </div>
     );
