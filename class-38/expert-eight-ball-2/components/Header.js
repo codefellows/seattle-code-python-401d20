ccb8329a0379a9ac2c8fe34acc70eb5fcd3e0ebe
Adam Owada <adamowada@gmail.com>
2023-02-08 11:28:28 -0800

    code updated

--- class-38/expert-eight-ball-2/components/Header.js
+++ class-38/expert-eight-ball-2/components/Header.js
@@ -1,8 +1,8 @@
-export default function Header(props) {
+export default function Header({ answeredQuestions }) {
     return (
         <header className="flex items-center justify-between p-4 bg-gray-500 text-gray-50">
             <h1>Expert 8 Ball</h1>
-            <p>{props.answeredQuestions.length} questions answered</p>
+            <p>{answeredQuestions.length} questions answered</p>
         </header>
     );
 }
