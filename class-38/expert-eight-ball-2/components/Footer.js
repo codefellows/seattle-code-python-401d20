34bfdd8f044f436dfe174dc0375472086334cb38
Adam Owada <adamowada@gmail.com>
2023-02-08 12:08:41 -0800

    code updated

--- class-38/expert-eight-ball-2/components/Footer.js
+++ class-38/expert-eight-ball-2/components/Footer.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer className="p-4 mt-8 bg-gray-500 text-gray-50">
+        <footer className="p-4 mt-8 bg-green-500 text-gray-50">
             <Link href="/careers">Careers</Link>
         </footer>
     );
