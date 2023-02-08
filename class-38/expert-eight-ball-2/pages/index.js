575e4ed1ee0b3484259ce2536cb91b94271ccb7d
Adam Owada <adamowada@gmail.com>
2023-02-08 11:39:25 -0800

    code updated

--- class-38/expert-eight-ball-2/pages/index.js
+++ class-38/expert-eight-ball-2/pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
 
                 <EightBall getLatestReply={getLatestReply} />
 
-                <ResponseTable answeredQuestions={answeredQuestions} />
+                <ResponseTable answeredQuestions={answeredQuestions} table/>
             </main>
 
             <Footer />
