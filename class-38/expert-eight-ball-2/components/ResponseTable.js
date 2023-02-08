5319266696c7438c69af39f8e77238a7f124f220
Adam Owada <adamowada@gmail.com>
2023-02-08 11:22:44 -0800

    code updated

--- class-38/expert-eight-ball-2/components/ResponseTable.js
+++ class-38/expert-eight-ball-2/components/ResponseTable.js
@@ -1,32 +1,32 @@
 export default function ResponseTable(props) {
 
     return (
-        props.answeredQuestions
-        <table className="w-1/2 mx-auto my-4">
-            <thead>
-                <tr>
-                    <th className="border border-gray-700">No.</th>
-                    <th className="border border-gray-700">Question</th>
-                    <th className="border border-gray-700">Response</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.answeredQuestions.map((item) => {
-                    return (
-                        <tr key={item.id}>
-                            <td className="pl-2 border border-gray-700">
-                                {item.id}
-                            </td>
-                            <td className="pl-2 border border-gray-700">
-                                {item.question}
-                            </td>
-                            <td className="pl-2 border border-gray-700">
-                                {item.reply}
-                            </td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+        props.answeredQuestions.length > 0 ?
+            <table className="w-1/2 mx-auto my-4">
+                <thead>
+                    <tr>
+                        <th className="border border-gray-700">No.</th>
+                        <th className="border border-gray-700">Question</th>
+                        <th className="border border-gray-700">Response</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.answeredQuestions.map((item) => {
+                        return (
+                            <tr key={item.id}>
+                                <td className="pl-2 border border-gray-700">
+                                    {item.id}
+                                </td>
+                                <td className="pl-2 border border-gray-700">
+                                    {item.question}
+                                </td>
+                                <td className="pl-2 border border-gray-700">
+                                    {item.reply}
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
     );
 }
