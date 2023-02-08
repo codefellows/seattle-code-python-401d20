15c4db7859ae8cf3008209193930b62329264686
Adam Owada <adamowada@gmail.com>
2023-02-08 11:30:03 -0800

    code updated

--- class-38/expert-eight-ball-2/pages/index.js
+++ class-38/expert-eight-ball-2/pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Header answeredQuestions={answeredQuestions} />
+            <Header answeredQuestions={answeredQuestions} name={"Adam"} />
 
             <main className=''>
                 <QuestionForm questionAskedHandler={questionAskedHandler} />
