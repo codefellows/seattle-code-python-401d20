31b271130df510ac96c4a7e211cfa8851f942136
Adam Owada <adamowada@gmail.com>
2023-02-08 10:34:48 -0800

    code updated

--- class-38/expert-eight-ball-2/components/Header.js
+++ class-38/expert-eight-ball-2/components/Header.js
@@ -1,5 +1,8 @@
 export default function Header() {
   return (
-
-  )
+    <header className='flex items-center justify-between p-4 bg-gray-500 text-gray-50'>
+      <h1>Expert 8 Ball</h1>
+      <p>{answeredQuestions.length} questions answered</p>
+    </header>
+  );
 }
